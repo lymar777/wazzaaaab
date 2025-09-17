9d64687ed1e5609a2fb5d3ddec232c117859aa47
lymar777 <miroslavlymar7@gmail.com>
2025-09-17 10:55:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ res= int(input("Xnj ye;yj? Ddtlbnt 1-4 \n 1 - Ghjwtyn")
 print(res.__class__)
 
 if == 1:
-  print("РАСЧЕТ ПЕРЕПЛАТЭ")
+  print("РАСЧЕТ ПРОЦЕНТА")
   percent = int(input("Введите годовой %:"))
-  money = int(input("Введите сумму займа:")
-  period = float(input("Введите период займа в годах:")
+  money = int(input("Введите сумму займа:"))
+  period = float(input("Введите период займа в годах:"))
 
 
-  res_0= ((money * percent) - money) * period
+  res_p = ((money * percent) - money) * period)
+print (f"От займа:{money} \n С годовым процентом:{percent} \n На период: {period} \n Переплата составит: {res_p} в рублях")
 
 print (f"От займа (money) \n")
 elif res=2: 
+
